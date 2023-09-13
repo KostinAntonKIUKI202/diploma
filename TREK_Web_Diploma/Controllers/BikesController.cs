9ecc405c36f5f43547fdac4060fed5c5fea6643f
Антон Костін <nitka5710@gmail.com>
2023-09-13 22:17:35 +0300

    Controller v1.0 (Intual part) Veiw v1.0 (Initual part)

---
+++ TREK_Web_Diploma/Controllers/BikesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TREK_Web_Diploma.Data;
+using TREK_Web_Diploma.Models.production;
+
+namespace TREK_Web_Diploma.Controllers
+{
+    public class BikesController : Controller
+    {
+        public readonly ApplicationDbContext _context;
+
+        public BikesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Bike> bikes = _context.BikeDB.ToList();
+            return View(bikes);
+        }
+    }
+}
---
+++ TREK_Web_Diploma/Controllers/StaffController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TREK_Web_Diploma.Data;
+
+namespace TREK_Web_Diploma.Controllers
+{
+    public class StaffController : Controller
+    {
+        public readonly ApplicationDbContext _context;
+        public StaffController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var staff = _context.StaffDB.ToList();
+            return View(staff);
+        }
+    }
+}
